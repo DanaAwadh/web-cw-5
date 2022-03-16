d20f9f2c8a8da2c57e917b144340e15c2f45a2de
dana awadh <danaawadh23@gmail.com>
2022-03-16 20:03:43 +0300

    d5

--- Script.js
+++ Script.js
@@ -1,2 +1,7 @@
+alert("dana")
 console.log(16);
 console.log("أنا في الصف الحادي عشر");
+let budget =20
+console.log(budget > 30 && buddget < 80)
+budget+= 30;
+console.log(budget > 30 && buddget < 80)
